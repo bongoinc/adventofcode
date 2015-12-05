7500ed035a492fd9ff8ffaea90d536d54baece27
P-O Larsson <p-o.larsson@contribe.se>
2015-12-06 00:57:55 +0100

    Added a solution to part2 of task 3.

--- 03/aoc03.py
+++ 03/aoc03.py
@@ -7,13 +7,20 @@ args = parser.parse_args()
 def main():
 	print("--- ADVENT OF CODE 2015 DAY 3 ---")
 
+	f = open(args.input, 'r')
+	data = f.read()
+
+	part1_answer = part1(data)
+	part2_answer = part2(data)
+
+	print("PART1: %d houses will receive at least one gift!" % part1_answer)
+	print("PART1: %d houses will receive at least one gift!" % part2_answer)
+
+def part1(data):
 	STARTING_POINT = "0,0"
 	coordinates = [STARTING_POINT]
 	x = 0
 	y = 0
-
-	f = open(args.input, 'r')
-	data = f.read()
 
 	for c in data:
 		if c == '^':
@@ -26,15 +33,52 @@ def main():
 			x -= 1
 
 		pos = str(x) + ',' + str(y)
-#		print("Pos: %s" % pos)
 
 		if pos not in coordinates:
 			coordinates.append(pos)
 
-#	print(coordinates)
-	print("PART1: %d houses will receive at least one gift!" % len(coordinates))
-#	print("PART2: The character position that first takes Santa to the basement is %d!" % first_basement)
+	return len(coordinates)
 
+def part2(data):
+	STARTING_POINT = "0,0"
+	coordinates = [STARTING_POINT]
+	santa_x = 0
+	santa_y = 0
+	robot_x = 0
+	robot_y = 0
+	santas_turn = True
+
+	for c in data:
+
+		if santas_turn:
+			if c == '^':
+				santa_y += 1
+			elif c == 'v':
+				santa_y -= 1
+			elif c == '>':
+				santa_x += 1
+			elif c == '<':
+				santa_x -= 1
+
+			pos = str(santa_x) + ',' + str(santa_y)
+		else:
+			if c == '^':
+				robot_y += 1
+			elif c == 'v':
+				robot_y -= 1
+			elif c == '>':
+				robot_x += 1
+			elif c == '<':
+				robot_x -= 1
+
+			pos = str(robot_x) + ',' + str(robot_y)
+
+		if pos not in coordinates:
+			coordinates.append(pos)
+
+		santas_turn = not santas_turn
+
+	return len(coordinates)
 
 if __name__ == '__main__':
     main()
