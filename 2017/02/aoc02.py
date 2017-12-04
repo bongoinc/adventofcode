ec78cd49d2fb8d0e9c6fa91a5c529323b4fbadf8
P-O Larsson <p-o.larsson@contribe.se>
2017-12-04 21:34:55 +0100

    Task 2017 2.2 Done

--- 2017/02/aoc02.py
+++ 2017/02/aoc02.py
@@ -9,17 +9,30 @@ args = parser.parse_args()
 def main():
   print("--- ADVENT OF CODE 2017 DAY 2 ---")
 
-  checksum = 0
+  checksum1 = 0
+  checksum2 = 0
 
   f = open(args.input, 'r')
 
   for row in f:
     rowlist = map(int, row.split(" "))
-#    print(rowlist)
-#    print("MAX: %d, MIN: %d, DIFF: %d" % (int(max(rowlist)), int(min(rowlist)), int(max(rowlist)) - int(min(rowlist))))
-    checksum += (int(max(rowlist)) - int(min(rowlist)))
+    checksum1 += (int(max(rowlist)) - int(min(rowlist)))
+    checksum2 += get_checksum_from_list(rowlist)
 
-  print("PART1: The checksum is %d!" % checksum)
+  print("PART1: The first checksum is %d!" % checksum1)
+  print("PART2: The second checksum is %d!" % checksum2)
+
+def get_checksum_from_list(numbers):
+  for num1 in numbers:
+    for num2 in numbers:
+      if num1 == num2: # Ignore same numbers
+        continue
+
+      high = max(num1, num2) # Get the highest number
+      low = min(num1, num2)  # Get the lowest number
+
+      if high % low == 0:
+        return high/low
 
 if __name__ == '__main__':
     main()
