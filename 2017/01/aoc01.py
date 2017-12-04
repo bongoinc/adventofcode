b22903e3b125b7eefd25a0588e1e44637c4aed50
P-O Larsson <p-o.larsson@contribe.se>
2017-12-04 17:49:53 +0100

    Added solution and test files for task 1.2

--- 2017/01/aoc01.py
+++ 2017/01/aoc01.py
@@ -9,26 +9,26 @@ args = parser.parse_args()
 def main():
   print("--- ADVENT OF CODE 2017 DAY 1 ---")
 
-  captcha = 0
+  captcha1 = 0
+  captcha2 = 0
 
   f = open(args.input, 'r')
   data = f.read()
   nums = list(data)
 
   for i, val in enumerate(nums):
-#    print("i = %d, item = %d" % (i, int(val)))
     if i == len(nums)-1:
-      captcha = calc_captcha(captcha, nums[i], nums[0])
+      captcha1 = calc_captcha(captcha1, nums[i], nums[0])
     else:
-      captcha = calc_captcha(captcha, nums[i], nums[i + 1])
+      captcha1 = calc_captcha(captcha1, nums[i], nums[i + 1])
 
-#  for pos in range(0, length):
-#    if pos == length-1:
-#      captcha = calc_captcha(captcha, int(nums[pos]), int(nums[0]))
-#    else:
-#      captcha = calc_captcha(captcha, int(nums[pos]), int(nums[pos + 1]))
+  for pos in range(0, len(nums)/2):
+    captcha2 = calc_captcha(captcha2, nums[pos], nums[pos + (len(nums)/2)])
 
-  print("PART1: The solution of the captcha is %d!" % captcha)
+  captcha2 = 2*captcha2
+
+  print("PART1: The solution of this captcha is %d!" % captcha1)
+  print("PART2: The solution of this captcha is %d!" % captcha2)
 
 def calc_captcha(summary, val1, val2):
   if val1 == val2:
