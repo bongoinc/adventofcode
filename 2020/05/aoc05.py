fb653d82421672ce908ae641ebf34da4ee551676
P-O <p-o.larsson@norrskensgatan12.se>
2020-12-06 13:33:12 +0100

    Finised day 5 2020, both tasks

--- 2020/05/aoc05.py
+++ 2020/05/aoc05.py
@@ -6,8 +6,8 @@ parser = argparse.ArgumentParser(description='AdventOfCode 2020 task 5')
 parser.add_argument('-i', '--input', help='Indata file, default data.txt', default='data.txt')
 args = parser.parse_args()
 
-UPPER = []
-LOWER = []
+UPPER = 'BR'
+LOWER = 'FL'
 
 def main():
     print('--- ADVENT OF CODE 2020 DAY 5 ---')
@@ -17,29 +17,39 @@ def main():
     for row in f:
         bps.append(row.strip())
 
-    first_answer = -1
-    second_answer = -1
+    seatids = get_seat_ids(bps)
 
+    first_answer = max(seatids)
+    second_answer = get_my_seat(sorted(seatids))
+
+    print('PART1: The highest seatid is %d!' % first_answer)
+    print('PART2: My seatid is %d!' % second_answer)
+
+def get_my_seat(s):
+    for i, id in enumerate(s):
+        if s[i+1] == id+2:
+            return id+1
+
+def get_seat_ids(bps):
+    ids = []
     for bp in bps:
         row, column = get_seat_from_boarding_pass(bp)
-        first_answer = max(first_answer, get_seat_id(row, column))
+        ids.append(get_seat_id(row, column))
+    return ids
 
-    print('PART1: The number of valid passports is %d!' % first_answer)
-    print('PART2: The number of valid passports is %d!' % second_answer)
 
 def get_position(str, l, u):
-    print("STR: {}, LOW: {}, HI: {}".format(str, l, u))
     lo = l
     hi = u
     for c in str:
         if c in UPPER:
-            lo = lo + (hi-lo+1)/2
+            lo = int(lo + (hi-lo+1)/2)
         elif c in LOWER:
-            hi = hi - (hi-lo+1)/2
+            hi = int(hi - (hi-lo+1)/2)
     return hi
 
 def get_seat_from_boarding_pass(bp):
-    r = get_position(bp[0,8], 0, 127)
+    r = get_position(bp[:7], 0, 127)
     c = get_position(bp[-3:], 0, 7)
     return r, c
 
