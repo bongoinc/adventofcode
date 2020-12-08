c677050ff926b82b19e7ab0dd51679e716ae3f2f
P-O Larsson <per.ola.larsson@softhouse.se>
2020-12-08 16:12:36 +0100

    Refactored day 7 2020, part 1

--- 2020/07/aoc07.py
+++ 2020/07/aoc07.py
@@ -4,7 +4,7 @@ import argparse
 from bag import Bag
 
 parser = argparse.ArgumentParser(description='AdventOfCode 2020 task 7')
-parser.add_argument('-i', '--input', help='Indata file, default data.txt', default='testdata.txt')
+parser.add_argument('-i', '--input', help='Indata file, default data.txt', default='data.txt')
 args = parser.parse_args()
 
 BAGS_CONTAINED = 'bags contain'
@@ -17,16 +17,14 @@ def main():
     f = open(args.input, 'r')
     for row in f:
         rules.append(row.strip())
-    print(rules)
 
     for r in rules:
         bags.append(decode_rule(r))
-    print(bags)
 
     first_answer = count_bags_that_might_contain_shiny_gold(bags)
     second_answer = -1
 
-    print('PART1: The sum of these counts are %d!' % first_answer)
+    print('PART1: The number of bags that can hold a shiny gold one are %d!' % first_answer)
     print('PART2: The sum of these counts are %d!' % second_answer)
 
 def count_bags_that_might_contain_shiny_gold(all_bags):
