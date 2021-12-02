fb9977183b9507b5f9053c7c3465919e5691ecbe
P-O <p-o.larsson@norrskensgatan12.se>
2021-12-02 20:25:40 +0100

    Day 1 2021, Part 2

--- 2021/01/aoc01.py
+++ 2021/01/aoc01.py
@@ -8,19 +8,41 @@ args = parser.parse_args()
 
 def main():
   print("--- ADVENT OF CODE 2021 DAY 1 ---")
+  indata = get_indata(args.input)
+  
+  print("PART1: %d measurements are bigger than the previous." % part_one(indata))
+  print("PART2: %d measurements are bigger than the previous." % part_two(indata))
+
+def get_indata(filename):
+  indata = []
+  f = open(filename, 'r')
+  for row in f:
+    indata.append(int(row.strip()))
+  return indata
+
+def part_one(indata):
   cnt = 0
   previous = -1
-
-  f = open(args.input, 'r')
-  for row in f:
-    current = int(row)
+  
+  for current in indata:
     if previous != -1:
       if current > previous:
         cnt += 1
     previous = current
-    
-  print("PART1: %d measurements are bigger than the previous." % cnt)
-#  print("PART2: The first reoccuring frequency is {} after {} repeats!".format(second_answer, repeats))
+  return cnt
+
+def part_two(indata):
+  cnt = 0
+  previous = -1
+  
+  for i in range(len(indata)-2):
+    current = indata[i] + indata[i+1] + indata[i+2]
+#    print("INDATA[%d,%d,%d]: %d" % (indata[i], indata[i+1], indata[i+2], current))
+    if previous != -1:
+      if current > previous:
+        cnt += 1
+    previous = current
+  return cnt
 
 if __name__ == '__main__':
     main()
